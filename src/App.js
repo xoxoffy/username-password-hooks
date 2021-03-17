81b576efe99b1559e99d08417fbad36adc45d8d3
Dominik <dominik.wozniewicz@gmail.com>
2021-03-17 05:28:29 +0100

    Implemented changing background color on button click

--- src/App.js
+++ src/App.js
@@ -6,31 +6,42 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [changeBackgroundColor, setChangeBackgroundColor] = useState(true);
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Enter username"
-        value={username}
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <input
-        type={showPassword ? "text" : "password"}
-        placeholder="Enter password"
-        value={password}
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
+    <div
+      style={{ backgroundColor: changeBackgroundColor ? "white" : "darkgray" }}
+    >
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Enter password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
 
-      <button type="button" onClick={() => setShowPassword(!showPassword)}>
-        Show/Hide password
-      </button>
-      <p>{username}</p>
-      <p>{showPassword ? password : "*".repeat(password.length)}</p>
+        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+          Show/Hide password
+        </button>
+        <button
+          type="button"
+          onClick={() => setChangeBackgroundColor(!changeBackgroundColor)}
+        >
+          Change background color
+        </button>
+        <p>{username}</p>
+        <p>{showPassword ? password : "*".repeat(password.length)}</p>
+      </div>
     </div>
   );
 };
